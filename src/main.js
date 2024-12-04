b3a3b7fc4b636183f84f02cf5dfab1ae5c3e2704
Alex Victor <alexvictorphb@gmail.com>
2024-12-03 23:49:25 -0300

    Início do Composables

--- src/main.js
+++ src/main.js
@@ -13,8 +13,15 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+import vuetify from './plugins/vuetify';
+
+// Supabase
+import supabase from './plugins/supabase';
+
 const app = createApp(App)
 
 registerPlugins(app)
 
+app.config.globalProperties.$supabase = supabase; // Torna o Supabase acessível globalmente
+app.use(vuetify);
 app.mount('#app')
